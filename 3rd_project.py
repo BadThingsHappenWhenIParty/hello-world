7dd34ff7087042411fb6ad4c169e7ef55a4cb2f9
BadThingsHappenWhenIParty <52837461+BadThingsHappenWhenIParty@users.noreply.github.com>
2019-07-13 21:10:30 +0200

    Update 3rd_project.py

--- 3rd_project.py
+++ 3rd_project.py
@@ -7,33 +7,68 @@ url = "https://volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"
 response = requests.get(url)
 
 soup = BS(response.text, "html.parser")
+# print(soup.prettify())
+# tabulka1 = soup.find_all('td', {'headers': 't1sb2'})
+# tabulka2 = soup.find_all('td', {'headers': 't2sb2'})
+# tabulka3 = soup.find_all('td', {'headers': 't3sb2'})
+# tabulka4 = soup.find_all('td', {'headers': 't4sb2'})
+tabulka_all = soup.find_all('td')
+tabulka_all_text = []
+tabulka_no_X = []
+for i in tabulka_all:
+    tabulka_all_text.append(i.text)
 
-tabulka1 = soup.find_all('td', {'headers': 't1sb2'})
-tabulka2 = soup.find_all('td', {'headers': 't2sb2'})
-tabulka3 = soup.find_all('td', {'headers': 't3sb2'})
-tabulka4 = soup.find_all('td', {'headers': 't4sb2'})
+for i in tabulka_all_text:
+    if i != 'X' and i != 'N':
+        tabulka_no_X.append(i)
+    else:
+        pass
 
-tabulka1_text = []
-tabulka2_text = []
-tabulka3_text = []
-tabulka4_text = []
+# print(tabulka_all_text)
+print(tabulka_no_X)
 
-for i in tabulka1:
-    tabulka1_text.append(i.text)
+numbers = []
+names = []
+for i in tabulka_no_X:
+    if "CZ" in i:
+        numbers.append(i)
+    else:
+        pass
 
-for i in tabulka2:
-    tabulka2_text.append(i.text)
+for i in tabulka_no_X:
+    if i in numbers:
+        pass
+    else:
+        names.append(i)
 
-for i in tabulka3:
-    tabulka3_text.append(i.text)
+print(numbers)
+print(names)
 
-for i in tabulka4:
-    tabulka4_text.append(i.text)
+my_dict = dict(zip(numbers, names))
 
-print(tabulka1_text)
-print(tabulka2_text)
-print(tabulka3_text)
-print(tabulka4_text)
+print(my_dict)
+
+# tabulka1_text = []
+# tabulka2_text = []
+# tabulka3_text = []
+# tabulka4_text = []
+#
+# for i in tabulka1:
+#     tabulka1_text.append(i.text)
+#
+# for i in tabulka2:
+#     tabulka2_text.append(i.text)
+#
+# for i in tabulka3:
+#     tabulka3_text.append(i.text)
+#
+# for i in tabulka4:
+#     tabulka4_text.append(i.text)
+
+# print(tabulka1_text)
+# print(tabulka2_text)
+# print(tabulka3_text)
+# print(tabulka4_text)
 
 cities = soup.findAll("h3", {"class": "kraj"})
 
